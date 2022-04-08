d4acf8ddd0bd36d8a0e38e291a3049d3ffaeae40
Alexandr Sanatar <allexvik@gmail.com>
2022-04-08 09:08:48 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import User from './User';
+import user from '../json/user.json';
+export const App = () => {
+  return (
+    <div>
+      <User
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+};
